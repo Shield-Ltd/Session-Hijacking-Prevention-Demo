4b2a61f1401b525b8c0034c1ffeee8ab8bfe92ae
Stebinjac <161051682+Stebinjac@users.noreply.github.com>
2026-01-28 23:16:59 +0530

    ft: added terms and conditons

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { jwtVerify } from 'jose';
 
-const PUBLIC_ROUTES = ['/login', '/signup', '/'];
+const PUBLIC_ROUTES = ['/login', '/signup', '/','/terms'];
 
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
